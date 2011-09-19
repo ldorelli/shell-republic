c5272e071f7fca9f3e5f420fc275818ec6f181b6
Denis Moreira dos Reis <denismr@gmail.com>
2011-09-19 18:40:13 -0300

    dup2

--- Executor.cpp
+++ Executor.cpp
@@ -1,2 +1,42 @@
 #include "Executor.hpp"
+#include <unistd.h>
 
+std::list<Job> Executor::getJobs() {
+    return jobs;
+}
+
+void Executor::run(Command* command, int& fdIn, int& fdOut, int& fdErr) {
+    const char ** execvector = command.getExecv();
+    Job job;
+    if (!jobs.empty()) {
+        job.jobid = jobs.rbegin().jobid + 1;
+    } else job.jobid = 1;
+    job.name = execvector[0];
+    
+    std::string in, out, err;
+    bool outap, errap;
+    in = command->getIn();
+    out = command->getOut();
+    err = command->getErr();
+    outap = command->getOutAppend();
+    errap = command->getErrAppend();
+    
+    if (!in.empty()) {
+        //pipezar
+    }
+    
+    if (!out.empty()) {
+        //pipezar
+    }
+    
+    if (!err.empty()) {
+        //pipezar
+    }
+    
+    
+    
+}
+
+void Executor::run(CommandLine cmdLine) {
+    
+}
--- Executor.hpp
+++ Executor.hpp
@@ -1,6 +1,28 @@
 #ifndef __Executor_HPP__
 #define __Executor_HPP__
 
-    
+#include <string>
+#include <vector>
+#include <list>
+#include <sys/types.h>
+#include "Command.hpp"
+#include "CommandLine.hpp"
+
+
+class Executor {
+public:
+    struct Job {
+        std::string name;
+        pid_t pid;
+        unsigned jobid;
+    };
+
+private:
+    std::list<Job> jobs;
+    void run(Command* command, int& fdIn = 0, int& fdOut = 1, int& fdErr = 2);
+public:
+    void run(CommandLine commandLine);
+    std::vector<Job> & getJobs();
+};
 
 #endif
