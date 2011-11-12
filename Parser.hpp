5077e7c2f2d061e27c789b9cfdc986f9bb8d1a7b
Denis Moreira dos Reis <denismr@gmail.com>
2011-11-12 21:25:59 -0200

    Commit

--- Parser.hpp
+++ Parser.hpp
@@ -4,7 +4,12 @@
 #include <string>
 #include "CommandLine.hpp"
 
-
+/**
+ * \brief Utilizado para converter a entrada do usuario em uma CommandLine
+ *
+ *        A linha de usuario deve ter a seguinte forma
+ *        <comando><paramentro>...<parametro>|...|<comando>...[< <entrada>][[1>|>|>>][2>]<saida>|&><saida>][&]
+ */
 class Parser {
     std::string line;
     unsigned index;
@@ -13,7 +18,14 @@ class Parser {
     
 public:
     Parser();
+    /**
+     * \return Le e interpreta uma linha de comando dada pelo usuario no stdin
+     */
     CommandLine* readCommandLine();
+    /**
+     * \return Verdadeiro caso esteja em uma nova linha de comando (nao necessariamente endl,
+     *         mas fim da pipeline por &
+     */
     bool newLine();
 };
 
